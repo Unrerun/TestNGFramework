1b1f088431cbce896f8a53cf0ffe1c3681ba0492
Unrerun <xxps@yandex.ru>
2019-08-22 16:14:23 +0400

    commit test

--- UiTest/src/test/java/UiTest.java
+++ UiTest/src/test/java/UiTest.java
@@ -1,21 +1,33 @@
-import okio.Timeout;
+//import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UiTest {
-    public WebDriver driver;
-    public String url = "https://trello.com/";
+    private WebDriver driver;
+    private String url = "https://trello.com/";
 
 
     @Test
-    public void testExample(){
+    public void testExample() throws InterruptedException {
     System.setProperty("webdriver.chrome.driver","C:\\chrome wd\\chromedriver.exe");
+//        ChromeDriverManager.getInstance().setup();
     driver = new ChromeDriver();
-    driver.get(url);
+    driver.get(this.url);
     Assert.assertEquals(driver.getTitle(), "Trello");
+        driver.findElement(By.xpath("/html/body/header/nav/div[2]/a[1]")).click();
+        wait(10);
+        Assert.assertEquals(driver.getTitle(), "Вход в Trello", "Тайтл отличается");
+//        driver.wait(10);
     driver.close();
     }
+
+//    @Test
+//    public void login(){
+//
+//        driver.close();
+//    }
 
 }
